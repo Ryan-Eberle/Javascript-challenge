9f34771b372e9db4c1071d4f6bda831ff13caf8f
Ryan-Eberle <ryanreberle@gmail.com>
2020-09-24 18:06:02 -0600

    Level-2 Upload

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -23,10 +23,43 @@ tableBuild(events);
 
 button.on("click", function dateInput(ufoEvent){
     tbody.html("")
-    var dateInfo = d3.select("#datetime").property("value");
-    console.log(dateInfo);
-    console.log(d3.event.target);
-    var dateSightings = data.filter((row)=>row.datetime == dateInfo);
-    console.log(dateSightings);
-    return tableBuild(dateSightings);
+    d3.event.preventDefault();
+    var dateTime = d3.select('#datetime').property('value');
+    var selectedCity = d3.select('#city').property('value').toLowerCase();
+    var selectedState = d3.select('#state').property('value').toLowerCase();
+    var selectedCountry = d3.select('#country').property('value').toLowerCase();
+    var selectedShape = d3.select('#shape').property('value').toLowerCase();
+    var wrongselection = ('What are they hiding?')
+    filterEvents = events;
+
+    if (dateTime) {
+        filterEvents = filterEvents.filter(row => row.datetime === dateTime);
+    }
+    if (selectedCity) {
+        filterEvents = filterEvents.filter(row => row.city === selectedCity);
+    }
+    if (selectedState) {
+        filterEvents = filterEvents.filter(row => row.state === selectedState);
+    }
+    if (selectedCountry) {
+        filterEvents = filterEvents.filter(row => row.country === selectedCountry);
+    }
+    if (selectedShape) {
+        filterEvents = filterEvents.filter(row => row.shape === selectedShape);
+    }
+    else {
+        wrongselection
+    }
+    filterEvents.forEach((alienEvent) => {
+        var row = tbody.append('tr');
+        Object.entries(alienEvent).forEach(([key,value]) => {
+            console.log(key,value);
+            var cell = row.append('td');
+            cell.text(value);
+        });
+        
+    });
+
+    // console.log(dateSightings);
+    // return tableBuild(dateSightings);
 });
